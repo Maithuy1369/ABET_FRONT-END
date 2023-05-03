e54ea1f390faef4aed2c84b120b51111100105dd
Maithuy1369 <127916018+Maithuy1369@users.noreply.github.com>
2023-05-04 04:04:33 +0700

    Update NavBar.vue

--- src/components/home/NavBar.vue
+++ src/components/home/NavBar.vue
@@ -149,7 +149,7 @@
             d="M3 8.812a4.999 4.999 0 0 1 2.578-4.375l-.485-.874A6 6 0 1 0 11 3.616l-.501.865A5 5 0 1 1 3 8.812z"
           />
         </svg>
-        <h3 class="title-text active" data-v-dfd06206="">Thoát</h3>
+        <h3 class="title-text active" data-v-dfd06206="">Đăng xuất</h3>
       </a>
 
       <!--]-->
